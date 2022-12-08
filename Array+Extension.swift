9f92be4bcf82b429cb0c50ab5ef18ef8e01b9da0
Amit Kumar Sahu <amitsahu13@gmail.com>
2022-12-08 20:53:50 +0530

    documentation updated

--- Array+Extension.swift
+++ Array+Extension.swift
@@ -1,5 +1,7 @@
 
-//Get Unique Elements from Array
+/**
+Get Unique Elements from Array
+*/
 extension Array where Element: Equatable {
     
     public func uniq() -> [Element] {
@@ -23,8 +25,14 @@ extension Array where Element: Equatable {
 }
 extension Array {
     func unique<T: Hashable>(map: ((Element) -> (T))) -> [Element] {
-        var set = Set<T>() //the unique list kept in a Set for fast retrieval
-        var arrayOrdered = [Element]() //keeping the unique list of elements but ordered
+        /**
+        the unique list kept in a Set for fast retrieval
+        */
+        var set = Set<T>() 
+        /**
+        keeping the unique list of elements but ordered
+        */
+        var arrayOrdered = [Element]() 
         for value in self {
             if !set.contains(map(value)) {
                 set.insert(map(value))
