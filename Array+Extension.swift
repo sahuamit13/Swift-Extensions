a390907f9963d7c0a548c1c45efcf130e1202c96
Amit Kumar Sahu <amitsahu13@gmail.com>
2022-12-08 20:35:40 +0530

    Unique with ordered

--- Array+Extension.swift
+++ Array+Extension.swift
@@ -21,3 +21,17 @@ extension Array where Element: Equatable {
         }
     }
 }
+extension Array {
+    func unique<T: Hashable>(map: ((Element) -> (T))) -> [Element] {
+        var set = Set<T>() //the unique list kept in a Set for fast retrieval
+        var arrayOrdered = [Element]() //keeping the unique list of elements but ordered
+        for value in self {
+            if !set.contains(map(value)) {
+                set.insert(map(value))
+                arrayOrdered.append(value)
+            }
+        }
+        
+        return arrayOrdered
+    }
+}
